45f7bdf653f758938a66e6ccdf13b02f0afd1d18
nicolasTawk <nicolas.tawk@std.balamand.edu.lb>
2024-04-25 15:29:55 +0300

    Initial Commit

---
+++ ads/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ads
+//
+//  Created by Nicolas Tawk on 25/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
